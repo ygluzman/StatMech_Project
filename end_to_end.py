a301b0be499f9a46b11b5b1aca47c7c4090ef456
pwindsor <57190604+pwindsor@users.noreply.github.com>
2019-11-02 13:22:44 -0500

    Update end_to_end.py

--- end_to_end.py
+++ end_to_end.py
@@ -10,9 +10,10 @@ Created on Fri Nov  1 21:53:48 2019
 import numpy as np
 from scipy.spatial import distance as dist
 
+#Calculate end to end distance where "ploy_array" is the coordinate polymer array and "N" is the number of segments in the polymer
 def end_to_end(poly_array, N):
   dist_array = dist.cdist(poly_array, poly_array, 'euclidean')
   end_to_end_dist = dist_array[0,N-1]
   return end_to_end_dist
 
-print(end_to_end(poly_array))
+print(end_to_end(poly_array, N))
