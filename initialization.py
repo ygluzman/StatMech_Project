e246c2b74c2a0fbecba3889c3f15ec15343489d1
pwindsor <57190604+pwindsor@users.noreply.github.com>
2019-11-02 13:00:21 -0500

    Add files via upload

---
+++ initialization.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 22:32:31 2019
+
+@author: peterwindsor
+"""
+
+#Import Dependencies
+import numpy as np
+
+#Define Polymer Coordinates where "N" is the number of segments and "l" is the segment length
+def initialization(N, l):
+    poly_coord = [(n,0) for n in range(0, N, l)]
+    poly_array = np.array(poly_coord)
+    return poly_array
+
+print(initialization(24, 1))
