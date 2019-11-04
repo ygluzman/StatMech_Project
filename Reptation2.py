f3c86d1eb31174561a4c36313a452a667f369338
mitra054 <57247332+mitra054@users.noreply.github.com>
2019-11-03 20:11:10 -0600

    Update Reptation2.py
    # Sample Input
    array = numpy.zeros((10, 2))
    for i in range(1, 10):
        array[i][0] = array[i - 1][0] + 1
        array[i][1] = array[i - 1][1]
    reptation(array, 10)

--- Reptation2.py
+++ Reptation2.py
@@ -8,55 +8,51 @@ import pylab
 import random 
 import matplotlib.pyplot as plt
   
-# defining the number of steps 
-s = 5
-n = s - 1
-steps = 5
-x = numpy.zeros(s) 
-y = numpy.zeros(s)
+# polymer= 2D polymer array s= size of the polymer
+def reptation(polymer, s):
+    n = s - 1
+    x = numpy.zeros(s) 
+    y = numpy.zeros(s)  
 
+    for i in range(1, s): 
+        x[i] = polymer[i][0]
+        y[i] = polymer[i][1]
+    print(polymer)
+    pylab.plot(x, y, 'ro')  
+    pylab.show() 
 
-for i in range(1, s): 
-    x[i] = x[i - 1] + 1
-    y[i] = y[i - 1]
-
-pylab.plot(x, y, 'ro')  
-pylab.show() 
-
-for i in range(1, steps):
+    
     p = x[n]
     q = y[n]
     val1 = random.randint(1, 4) 
     if val1 == 1: 
         x[n] = x[n] + 1
         y[n] = y[n] 
-        for i in range(1, n-1):
+        for i in range(0, n):
             if (x[n] == x[i] and y[n] == y[i]):
                 x[n] = p
                 y[n] = q
     elif val1 == 2:
         x[n] = x[n] - 1
         y[n] = y[n] 
-        for i in range(1, n-1):
+        for i in range(0, n):
             if (x[n] == x[i] and y[n] == y[i]):
                 x[n] = p
                 y[n] = q
     elif val1 == 3:
         x[n] = x[n]
         y[n] = y[n] + 1
-        for i in range(1, n-1):
+        for i in range(0, n):
             if (x[n] == x[i] and y[n] == y[i]):
                 x[n] = p
                 y[n] = q
     elif val1 == 4:
         x[n] = x[n]
         y[n] = y[n] - 1
-        for i in range(1, n-1):
+        for i in range(0, n):
             if (x[n] == x[i] and y[n] == y[i]):
                 x[n] = p
-                y[n] = q
-    pylab.plot(x, y, 'ro')  
-    pylab.show()
+                y[n] = q 
     r = x[n]
     t = y[n]
     x[n] = p
@@ -66,18 +62,11 @@ for i in range(1, steps):
         y[i] = y[i + 1]
     x[n] = r
     y[n] = t
-    pylab.plot(x, y, 'ro')  
-    print(x, y)
+    for i in range(0, s): 
+        polymer[i][0] = x[i]
+        polymer[i][1] = y[i]
+    print(polymer)
+    pylab.plot(x, y, 'ro')
     pylab.show()
+    return polymer  
     
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
