eeed4763e768a6a91503f8812bf098160405d45f
dking-github <danielking4812@gmail.com>
2019-11-23 12:00:17 -0600

    Yay

--- master_script.py
+++ master_script.py
@@ -50,6 +50,9 @@ def prepare_gif_process():
         pass
 
 def plot(array,title,output_directory):
+    """
+    Makes a plot of the values of a 1D numpy array and saves with given title in output directory
+    """
     move_numbers = np.linspace(0, array.shape[0], array.shape[0])
     plt.clf()
     plt.plot(move_numbers, array, 'o', markersize=0.5, color='black');
@@ -57,6 +60,9 @@ def plot(array,title,output_directory):
     plt.savefig(output_directory + "/" + title + '.png')
 
 def avg_std(array,n_points):
+    """
+    Takes avg, std. dev., and std. error of mean over the last n points
+    """
     last_points_array = array[-1:-n_points:-1]
     avg = np.mean(last_points_array)
     std_dev = last_points_array - avg
@@ -81,6 +87,10 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamilar=False,temp=None
     field -- field strength, if lamilar is turned on
     output_directory -- where you would like to output the run
     """
+
+    ######################
+    ### Initialization ###
+    ######################
 
     if output_directory == None:
         print("Error! Output directory not specified. Exiting...")
@@ -114,18 +124,25 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamilar=False,temp=None
     if lamilar == True:
         charges = generate_coblock_charges(poly.shape[0])
 
+
+    ########################
+    #### Run Simulation ####
+    ########################
+
     #Run algorithm:
     for i in range(0,n_moves+1):
 
+        #Write original polymer to memory if laminar is on:
         if lamilar == True:
             poly1 = poly
 
+        #Make a move:
         if move_type == 'reptation':
             poly = reptation(poly)
         else: 
             poly = pivot(poly)
 
-        #lamilar called with polymer1, polymer2, charges, temp, field=1
+        #If laminar is on, compare energies of original and moved polymers:
         if lamilar == True:
             poly2 = poly
             poly, energy = lamilar_test(poly1,poly2,charges,temp,field=field)
@@ -140,11 +157,9 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamilar=False,temp=None
         #Append observables to array
         energies = np.append(energies,np.array([energy]))
         gyradii = np.append(gyradii,np.array([gyradius]))
-        # print(gyradii)
         end2ends = np.append(end2ends,np.array([end2end]))
-        # print(end2ends)
 
-        #Save images only if requested
+        #Save images for gif upon request
         if generate_gif == True:
             if lamilar == True:
                 img = grid.plot_poly(poly,energy=energy,gyradius=np.round(gyradius,2),end2end=np.round(end2end,2),lamilar=True,charges=charges)
@@ -152,24 +167,25 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamilar=False,temp=None
                 img = grid.plot_poly(poly,energy=energy,gyradius=np.round(gyradius,2),end2end=np.round(end2end,2))
             save_img(img)
 
-
-    #Debug
-    # print(gyradii)
-    # print(end2ends)
+    #########################################
+    #### Plot and Calculate Observables  ####
+    #########################################
 
     #Generate plots of observables over the algorithm
     plot(energies,"Energy",output_directory)
     plot(gyradii,"Radius of Gyration",output_directory)
     plot(end2ends,"End to End Length",output_directory)
 
-    #Calculate averages and standard deviations of observables from the last 500 moves
-    #Edit later base off of observed equilibration time
+    #Calculate avg, std. dev., and sem over the last n/2 points
     points_sampled = n_moves//2
     avg_energy, std_energy, sem_energy = avg_std(energies,points_sampled)
     avg_gyradius, std_gyradius, sem_gyradius = avg_std(gyradii,points_sampled)
     avg_end2end, std_end2end, sem_end2end = avg_std(end2ends,points_sampled)
 
-    #Print mean and standard deviation to output file
+    ###########################
+    #### Write Output File ####
+    ###########################
+
     output_name = output_directory + "/output.txt"
 
     #Remove previous output if it exists
@@ -178,7 +194,7 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamilar=False,temp=None
     except FileNotFoundError:
         pass
 
-    #Write to output file    
+    #Write output file:    
     with open(output_directory + "/output.txt", 'w+') as file:
         file.write("POLYSIM" + "\n")
         file.write("A Software by Abhishek, Daniel, Yogev, Peter, and Caini" + "\n")
@@ -217,16 +233,21 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamilar=False,temp=None
         file.write("\n")
         file.write("\n")
 
-    #Generate gif with terminal command if requested
-    #This will take a while if you have many images (~2-3 minutes for 1000 images)
+    #######################
+    #### Generate Gif  ####
+    #######################
+
     if generate_gif:
         print("Simualation done! Generating gif (this might take a while (2-3 minutes))...")
         make_gif(n_moves,output_directory)
 
 
 def main():
-    #Seed if you want
-    # np.random.seed(34)
+    """
+    Runs a simulation with user-specified settings below
+    Values in output are taken over the last n_moves//2 points, so be sure to equilibrate at least
+    over twice the time it takes your system to equilibrate
+    """
 
     ########################
     ###### IMPORTANT #######
@@ -244,12 +265,8 @@ def main():
     temp = 10
     field_strength = 1
 
-    #Generate a gif? Takes 2-3 minutes more on execution
-    #Also takes a lot of time to generate images
+    #Generate a gif? Takes 2-3 minutes more in execution
     generate_gif = False
-
-    #Note: Values in output are from sample of n_moves//2 points. Be sure to equilibrate at least over
-    # twice the time it takes your system to equilibrate
 
     #####################
     # STOP EDITING HERE #
@@ -265,8 +282,9 @@ def main():
     global img_directory
     img_directory = output_directory + "/generated_images"
 
-    #run(move_type,n_moves,length_poly,generate_gif=False,lamilar=False,temp=None,field=1)
-    run(move_type = move_type ,n_moves = n_moves, length_poly = length_poly, generate_gif = generate_gif, lamilar = laminar, temp = temp, field = field_strength, output_directory = output_directory)
+    run(move_type = move_type ,n_moves = n_moves, length_poly = length_poly, generate_gif = generate_gif, lamilar = laminar,
+         temp = temp, field = field_strength, output_directory = output_directory)
+
 
 if __name__ == "__main__":  
     main()
