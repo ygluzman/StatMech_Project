d89f0eaa64e71a482ad4d7b7c1fba47d6cc98022
dking-github <danielking4812@gmail.com>
2019-11-26 16:31:54 -0600

    Added support for multi-runs!

--- master_script.py
+++ master_script.py
@@ -17,6 +17,14 @@ from misc import end_to_end, radius_of_gyration, init
 img_number = 0
 img_directory = "generated_images"
 
+avg_energy = 0
+avg_gyradius = 0
+avg_gyradius_x = 0
+avg_gyradius_y = 0
+avg_end2end = 0
+avg_end2end_x = 0
+avg_end2end_y = 0
+
 def save_img(img):
     """
     Saves image to "generated_images" directory
@@ -68,7 +76,7 @@ def avg_std(array,n_points):
     std_dev = std_dev ** 2
     std_dev = np.mean(std_dev)
     std_dev = np.sqrt(std_dev)
-    sem = std_dev/n_points
+    sem = std_dev/np.sqrt(n_points)
     return avg, std_dev, sem
 
 
@@ -86,6 +94,15 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamellar=False,temp=Non
     field -- field strength, if lamellar is turned on
     output_directory -- where you would like to output the run
     """
+
+    #Declaring global variables for multi-runs:
+    global avg_energy
+    global avg_gyradius
+    global avg_gyradius_x
+    global avg_gyradius_y
+    global avg_end2end
+    global avg_end2end_x
+    global avg_end2end_y
 
     ######################
     ### Initialization ###
@@ -233,7 +250,10 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamellar=False,temp=Non
             file.write("Field Strength: " + str(field) + "\n")
         else:
             file.write("OFF" + "\n")
+        if lamellar == True:
+            file.write("Temperature: " + str(temp) + "\n")
 
+        file.write("Length of Polymer: " + str(length_poly) + "\n")
         file.write("Moves in Simualation: " + str(n_moves) + "\n")
         file.write("Points Sampled: " + str(points_sampled) + "\n")
 
@@ -288,6 +308,107 @@ def run(move_type,n_moves,length_poly,generate_gif=False,lamellar=False,temp=Non
         print("Simualation done! Generating gif (this might take a while (2-3 minutes))...")
         make_gif(n_moves,output_directory)
 
+def multi_run(n_sims,move_type,n_moves,length_poly,generate_gif=False,lamellar=False,temp=None,field=1,output_directory=None):
+    
+    #Global variables to get from sims:
+    energies = np.empty(0)
+    gyradii = np.empty(0)
+    gyradii_x = np.empty(0)
+    gyradii_y = np.empty(0)
+    end2ends = np.empty(0)
+    end2ends_x = np.empty(0)
+    end2ends_y = np.empty(0)
+
+    global avg_energy
+    global avg_gyradius
+    global avg_gyradius_x
+    global avg_gyradius_y
+    global avg_end2end
+    global avg_end2end_x
+    global avg_end2end_y
+
+    for i in range(1,n_sims+1):
+        run_directory = output_directory + '/sim_' + str(i)
+        os.mkdir(run_directory)
+
+        run(move_type = move_type ,n_moves = n_moves, length_poly = length_poly, generate_gif = generate_gif, lamellar = lamellar,
+         temp = temp, field = field, output_directory = run_directory)
+        
+        #Append outputs to arrays
+        energies = np.append(energies,np.array([avg_energy]))
+        gyradii = np.append(gyradii,np.array([avg_gyradius]))
+        gyradii_x = np.append(gyradii_x,np.array([avg_gyradius_x]))
+        gyradii_y = np.append(gyradii_y,np.array([avg_gyradius_y]))
+        end2ends = np.append(end2ends,np.array([avg_end2end]))
+        end2ends_x = np.append(end2ends_x,np.array([avg_end2end_x]))
+        end2ends_y = np.append(end2ends_y,np.array([avg_end2end_y]))
+
+    avg_energy, std_energy, sem_energy = avg_std(energies,len(energies))
+    avg_gyradius, std_gyradius, sem_gyradius = avg_std(gyradii,len(energies))
+    avg_gyradius_x, std_gyradius_x, sem_gyradius_x = avg_std(gyradii_x,len(energies))
+    avg_gyradius_y, std_gyradius_y, sem_gyradius_y = avg_std(gyradii_y,len(energies))
+    avg_end2end, std_end2end, sem_end2end = avg_std(end2ends,len(energies))
+    avg_end2end_x, std_end2end_x, sem_end2end_x = avg_std(end2ends_x,len(energies))
+    avg_end2end_y, std_end2end_y, sem_end2end_y = avg_std(end2ends_y,len(energies))
+
+    with open(output_directory + "/output.txt", 'w+') as file:
+        file.write("POLYSIM" + "\n")
+        file.write("A Software by Abhishek, Daniel, Yogev, Peter, and Caini" + "\n")
+        file.write("\n")
+
+        file.write(str(n_sims) + " SIMULATIONS ACHIEVED" + "\n")
+        file.write("SETTINGS:" + "\n")
+        file.write("########################" + "\n")
+        file.write("Move type: " + move_type + "\n")
+        file.write("Lamellar field: ")
+        if lamellar == True:
+            file.write("ON" + "\n")
+            file.write("Field Strength: " + str(field) + "\n")
+        else:
+            file.write("OFF" + "\n")
+        if lamellar == True:
+            file.write("Temperature: " + str(temp) + "\n")
+
+        file.write("Length of Polymer: " + str(length_poly) + "\n")
+
+        file.write("Moves in Each Simualation: " + str(n_moves) + "\n")
+        file.write("Points Sampled in Each Simulation: " + str(len(energies)) + "\n")
+        file.write("\n")
+
+        file.write("Average values over " + str(n_sims) + " simulations" + ":\n")
+        file.write("########################" + "\n")
+        file.write("Average energy: " + str(avg_energy) + "\n")
+        file.write("\n")
+        file.write("Average radius of gyration: " + str(avg_gyradius) + "\n")
+        file.write("Average radius of gyration x: " + str(avg_gyradius_x) + "\n")
+        file.write("Average radius of gyration y: " + str(avg_gyradius_y) + "\n")
+        file.write("\n")
+        file.write("Average end to end: " + str(avg_end2end) + "\n")
+        file.write("Average end to end x: " + str(avg_end2end_x) + "\n")
+        file.write("Average end to end y: " + str(avg_end2end_y) + "\n")
+
+        file.write("\n")
+        file.write("Standard error over " + str(n_sims) + " simulations" + ":\n")
+        file.write("########################" + "\n")
+        file.write("Std error of energy: " + str(sem_energy) + "\n")
+        file.write("\n")
+
+        file.write("Std error of radius of gyration: " + str(sem_gyradius) + "\n")
+        file.write("Std error of radius of gyration x: " + str(sem_gyradius_x) + "\n")
+        file.write("Std error of radius of gyration y: " + str(sem_gyradius_y) + "\n")
+
+        file.write("\n")
+        file.write("Std error of end to end distance: " + str(sem_end2end) + "\n")
+        file.write("Std error of end to end distance x: " + str(sem_end2end_x) + "\n")
+        file.write("Std error of end to end distance y: " + str(sem_end2end_y) + "\n")
+        file.write("########################" + "\n")
+        file.write("\n")
+
+        file.write("NORMAL TERMINATION" + "\n")
+        file.write("THANK YOU FOR USING OUR SOFTWARE!" + "\n")
+        file.write("\n")
+        file.write("\n")
+        file.write("\n")
 
 def main():
     """
@@ -311,9 +432,10 @@ def main():
     lamellar = True
     temp = 50
     field_strength = 1
+    n_sims = 5
 
     #Generate a gif? Takes 2-3 minutes more in execution
-    generate_gif = True
+    generate_gif = False
 
     #####################
     # STOP EDITING HERE #
@@ -332,7 +454,7 @@ def main():
     img_directory = output_directory + "/generated_images"
 
     #Execute the run
-    run(move_type = move_type ,n_moves = n_moves, length_poly = length_poly, generate_gif = generate_gif, lamellar = lamellar,
+    multi_run(n_sims = n_sims, move_type = move_type ,n_moves = n_moves, length_poly = length_poly, generate_gif = generate_gif, lamellar = lamellar,
          temp = temp, field = field_strength, output_directory = output_directory)
 
 
